f21824c18ab40eff4423c718c23e185b093ce681
@hexlet/cli <support@hexlet.io>
2023-03-29 02:09:04 +0300

    submit java-oop-ru/reflections

--- java-oop-ru/reflections/src/main/java/exercise/Validator.java
+++ java-oop-ru/reflections/src/main/java/exercise/Validator.java
@@ -1,30 +1,70 @@
 package exercise;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.lang.reflect.Method;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.stream.Collectors;
 
 // BEGIN
-public class Validator {
-    public static List<String> validate(Object obj) {
-
-        Address address = (Address) obj;
-        for (Field m : obj.getClass().getDeclaredFields()) {
-            if (m.isAnnotationPresent(NotNull.class)) {
-                Annotation[] notNull = m.getAnnotations();
-                System.out.println(Arrays.toString(notNull));
-
-            }
-        }
-        return new ArrayList<>();
+class Validator {
+    public static List<String> validate(Object instance) {
+        List<Field> fields = List.of(instance.getClass().getDeclaredFields());
+        return fields.stream()
+                .filter(field -> field.isAnnotationPresent(NotNull.class))
+                .filter(field -> {
+                    Object value;
+                    try {
+                        field.setAccessible(true);
+                        value = field.get(instance);
+                        field.setAccessible(false);
+                    } catch (Exception e) {
+                        throw new RuntimeException(e);
+                    }
+                    return value == null;
+                })
+                .map(Field::getName)
+                .collect(Collectors.toList());
     }
 
-    public static void main(String[] args) {
-        var address = new Address("aaa", null, "ccc", "ddd", "eee");
-        validate(address);
+    public static Map<String, List<String>> advancedValidate(Object instance) {
+        List<Field> fields = List.of(instance.getClass().getDeclaredFields());
+        Map<String, List<String>> validationErrors = new HashMap<>();
+        fields.stream()
+                .filter(field -> field.isAnnotationPresent(NotNull.class) || field.isAnnotationPresent(MinLength.class))
+                .forEach(field -> {
+                    String fieldName = field.getName();
+                    List<String> errors = getErrorsForField(field, instance);
+                    if (!errors.isEmpty()) {
+                        validationErrors.put(fieldName, errors);
+                    }
+                });
+        return validationErrors;
+    }
+
+    public static List<String> getErrorsForField(Field field, Object instance) {
+        List<String> errors = new ArrayList<>();
+        String value;
+
+        try {
+            field.setAccessible(true);
+            value = (String) field.get(instance);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        if (field.isAnnotationPresent(NotNull.class) && value == null) {
+            errors.add("can not be null");
+        }
+
+        if (field.isAnnotationPresent(MinLength.class)) {
+            int minLength = field.getAnnotation(MinLength.class).minLength();
+            if (value == null || value.length() < minLength) {
+                errors.add("length less than " + minLength);
+            }
+        }
+        return errors;
     }
 }
 // END
